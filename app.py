7b9b350b7d59f69c994e5d8421086f5370d3c0c7
SilvioHC <silviohenriquecruzdasilva@gmail.com>
2022-03-28 17:15:52 -0300

    get video url

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask,render_template,request,redirect,url_for
 from pytube import YouTube
 
 app = Flask(__name__)
+#__________ FUNCTIONS ___________
+def number_to_string(number):
+    number = str(number)[::-1]
+    number = '.'.join([number[num:num+3]for num in range(0,len(number),3)])
+    number = number[::-1]
+    if '.' in number:
+        number = number[:number.rfind('.')] + ',' + number[number.rfind('.')+1:]
+    return number
 
 
 @app.route('/')
@@ -12,6 +20,8 @@ def index():
 def url_download():
     url = request.args.get('url')
     if url:
-        return render_template('download.html')
+        video = YouTube(url)
+        data = {'titulo':video.title,'thumbnail':video.thumbnail_url,'time':video.length,'data':str(video.publish_date),'views':number_to_string(video.views),'autor':video.author}
+        return render_template('download.html',**data)
     else:
         return redirect(url_for('index'))
