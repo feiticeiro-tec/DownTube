3eb543b09c6444f7d0429c54d855bee060a58ecd
SilvioHC <silviohenriquecruzdasilva@gmail.com>
2022-03-28 23:02:57 -0300

    ...

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ def download(url,resolution,format_output):
                 video.streams.get_highest_resolution().download(app.output)
             else:
                 video.streams.get_lowest_resolution().download(app.output)
-        return video
+        return True,video
     except:
-        return False
+        return False,None
 
 #__________ ROUTES ___________
 
